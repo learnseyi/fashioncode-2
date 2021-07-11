40726842eff20d325dfc404ecc7a237996a8df7a
Seyi Agboola <learnseyi@gmail.com>
2021-07-11 18:32:52 -0500

    created mobile navigation menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 
 const stickyTop = document.querySelector('.sticky-top');
 const navList = document.querySelectorAll('.main-nav li a');
+const mainNav = document.querySelector(".main-nav");
 const head = document.querySelector('.top-message');
 const slides = document.querySelectorAll('.carousel-items')
 const nextSlide = document.querySelector(' .forward')
@@ -13,6 +14,7 @@ const prevReviewBtn = document.querySelector('.previousBtn');
 const indicators= document.querySelectorAll('.indicator');
 const root = head.getBoundingClientRect().height;
 const offset = stickyTop.getBoundingClientRect().height - root;
+const subNavHeight = subNav.getBoundingClientRect().height;
 
 // GLOBAL OBJECTS
 
@@ -78,11 +80,32 @@ const isAnimate = () => {
 
 document.addEventListener('scroll', isAnimate)
 
+/* ----------------------------------------*/
+// mobile navigation menu configuration
+const offsetPos = stickyTop.getBoundingClientRect().height
+scrollY >= root ? mainNav.style.top = `${subNavHeight - (subNavHeight / 2)}px`: mainNav.style.top = `${offset}px`;
+document.addEventListener('scroll',()=>{
+    scrollY >= root ? mainNav.style.top = `${subNavHeight - (subNavHeight / 2)}px`: mainNav.style.top = `${offset}px`;
+    
+})
+
+// page navigation
+const scrollToSection = (e)=>{
+    e.preventDefault();
+    const element = document.getElementById(e.target.getAttribute('href'))
+    const elementPosition = element.offsetTop - offset
+    window.scrollTo({ top: elementPosition, behavior: 'smooth' });
+}
+navList.forEach(nav => {
+    nav.addEventListener('click', scrollToSection)
+})
+
+/*----------------------------------------------*/
+/*----------------------------------------------*/
+
 // initializing main carousel instance
 const mainCarousel = new Carousel(slides)
-window.addEventListener('load',()=>{
-    mainCarousel.updateCarousel()
-})
+
 nextSlide.addEventListener('click',(e)=>{
     e.preventDefault();
     mainCarousel.slideForward();
@@ -113,98 +136,6 @@ reviewSlider.carouselIndicator();
 
 
 
-// // CAROUSEL ANIMATION
-// let currentSlide = 0;
-// const sliderNumber = slides.length;
-
-//  <-- update slide -->
-// const showSlide = (sliderIndex)=>{
-//     slides.forEach((slide, i) => {
-//         slide.style.transform = `translateX(${100 * (i - sliderIndex)}%)`;
-//     })
-// }
-// initialize slider to index zero onPageload
-// showSlide(0) 
-//  <-- automatic slide show function -->
-// ??  <-- There is code duplication here -->
-// const getSliderIndex = ()=>{
-//     if(currentSlide === sliderNumber - 1){
-//         currentSlide = 0;
-//     }else{
-//         currentSlide++;
-//     }
-//     return currentSlide;
-// }
-// const autoSlideShow = ()=>{
-//     setInterval(()=>{
-//         showSlide(getSliderIndex())
-        
-//     }
-//        ,3000 ) 
-// }
-// // autoSlideShow()
-// // <--slider forward control -->
-// const showNextSlide = ()=>{
-//     if(currentSlide === sliderNumber - 1){
-//         currentSlide = 0;
-//     }else{
-//         currentSlide++;
-//     }
-// showSlide(currentSlide);
-// }
-// // <-- slider previous control -->
-// const showPrevSlide = ()=>{
-//     if(currentSlide > 0 ){
-//         currentSlide --;
-//     }else{
-//         currentSlide = 0;
-//     }   
-//     showSlide(currentSlide)
-// }
-
-// //  <-- REVIEWS SECTION SLIDER ANIMATION -->
-// // review slider
-// const reviewNumber = reviews.length;
-// // update slider
-// const showReview = (reviewIndex)=>{
-//     reviews.forEach((review,i) =>{
-//         review.style.transform =`translateX(${100 * (i - reviewIndex)}%)`
-//     })
-// }
-// showReview(0)
-// // <-- next review control -->
-// const showNextReview = ()=>{
-//     if(currentSlide === reviewNumber - 1){
-//         currentSlide = 0; 
-//     }else{
-//         currentSlide++;
-//     }
-//     showReview(currentSlide)
-// }
-// //  <-- previous review control -->
-// const showPreviousReview = ()=>{
-//     if(currentSlide > 0){
-//         currentSlide--;
-//     }else{
-//         currentSlide = 0;
-//     }
-//     showReview(currentSlide)
-// }
-// EVENT LISTENERS
-// nextSlide.addEventListener('click',showNextSlide)
-// prevSlide.addEventListener('click',showPrevSlide)
-// nextReviewBtn.addEventListener('click',showNextReview);
-// prevReviewBtn.addEventListener('click',showPreviousReview);
-// scroll to function
-const scrollToSection = (e)=>{
-    e.preventDefault();
-    const element = document.getElementById(e.target.getAttribute('href'))
-    const elementPosition = element.offsetTop - offset
-    window.scrollTo({ top: elementPosition, behavior: 'smooth' });
-}
-navList.forEach(nav => {
-    nav.addEventListener('click', scrollToSection)
-})
 
 
 
